be5b2da7b66c510b0b3c8617a6f428ab50f58220
Brendon Body <bbody.project@gmail.com>
2019-04-23 23:45:07 +1000

    Clean up code

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,10 @@ import router from './router';
 import store from './store';
 import Clipboard from 'v-clipboard';
 import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
-import 'vue-calendar-heatmap/dist/vue-calendar-heatmap.css';
-const HeatMap = require('vue-calendar-heatmap');
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Clipboard);
 Vue.use(Vuetify);
-Vue.use(HeatMap);
 
 Vue.config.productionTip = false;
 
